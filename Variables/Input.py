e7f2c785fb9f8d27c36fa8904ef0bb7003e30ae9
Ajoke Keshiro <ajkeshy@gmail.com>
2026-02-01 00:43:26 +0100

    inut learning

--- Variables/Input.py
+++ Variables/Input.py
@@ -1,1 +1,11 @@
-input
+# 1. Python asks a question and waits
+favorite_language = input("What language are you learning? ")
+
+# 2. It stores your answer in the variable
+print("That's right! " + favorite_language + " is the best choice.")
+
+# 3. Let's try a number
+age = input("How many years old are you? ")
+
+# This next line will work because we are just printing text
+print("You are " + age + " years old.")
