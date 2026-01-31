8f935a7b79408f5e72bed01d3d233aa60a72169b
Ajoke Keshiro <ajkeshy@gmail.com>
2026-02-01 00:05:05 +0100

    Variable exploration

--- Variables/Basics.py
+++ Variables/Basics.py
@@ -1,1 +1,8 @@
-Python!
+# This is a variable holding a text (a String)
+goal = "Professional Python Developer"
+
+# This is a variable holding a number (an Integer)
+months = 6
+
+# This line combines them and prints them out
+print("My goal is to become a " + goal + " in " + str(months) + " months!")
