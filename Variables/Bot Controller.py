6a39ad0be1841637ee771588aa2cc97873b0682a
Ajoke Keshiro <ajkeshy@gmail.com>
2026-02-03 22:22:55 +0100

    Adding while True to the loop

--- Variables/Bot Controller.py
+++ Variables/Bot Controller.py
@@ -1,21 +1,26 @@
-#A script that simulates a simple robot
+#A script that simulates an always-on robot
+print("🤖 Robot System Online. ")
+print("Type 'quit' to exit the program.")
 
-#Ask for user input
-command = input("Enter a command for the robot (forward, backward, left, right, start, stop): ").lower()
-#Process the commands
-if command == "start":
-    print("Robotic arm is moving...")
-elif command == "forward":
-    print("Robot moving forward.")
-elif command == "backward":
-    print("Robot moving backward.")
-elif command == "left":
-    print("Robot turning left.")
-elif command == "right":
-    print("Robot turning right.")
-elif command == "stop":
-    print("Robotic arm has halted.")
-elif command == "wait":
-    print("Robot is in standby mode.")
-else:
-    print("Error: Unknown command.")
+while True:
+    command = input("\nCommand > ").lower()
+    if command == "quit":
+        print("Shutting down robot system. Goodbye!")
+        break
+    elif command == "start":
+        print("Robotic arm is moving...")
+    elif command == "forward":
+        print("Robot moving forward.")
+    elif command == "backward":
+        print("Robot moving backward.")
+    elif command == "left":
+        print("Robot turning left.")
+    elif command == "right":
+        print("Robot turning right.")
+    elif command == "stop":
+        print("Robotic arm has halted.")
+    elif command == "wait":
+        print("Robot is in standby mode.")
+    else:
+        print("Error: Unknown command.")
+# The robot will respond to commands until 'quit' is entered
